4bd33d5119c0226892ea59c7b6cf243b52ee9369
alex wennerberg <alex@alexwennerberg.com>
2021-08-01 14:11:03 -0700

    Add basic HTML sanitation

--- src/filters.rs
+++ src/filters.rs
@@ -7,14 +7,32 @@ pub fn get_header(email: &&ParsedMail, header: &str) -> askama::Result<String> {
         .unwrap_or("".to_string()))
 }
 
-// text/*
+// NOTE this function is currently unsafe
 pub fn get_body(email: &&ParsedMail) -> askama::Result<String> {
-    // TODO mimetypes
-    let is_text = email.ctype.mimetype.starts_with("text/");
-    if email.subparts.len() > 0 {
-        return Ok(email.subparts[0].get_body().unwrap_or("".to_string()));
+    let core_email = email.subparts.get(0).unwrap_or(email);
+
+    #[cfg(feature = "html")]
+    {
+        use ammonia;
+        use std::collections::HashSet;
+        use std::iter::FromIterator;
+        // TODO dont initialize each time
+        // TODO sanitize id, classes, etc.
+        let tags = HashSet::from_iter(vec!["a", "b", "i", "br", "p", "span", "u"]);
+        if core_email.ctype.mimetype == "text/html" {
+            let a = ammonia::Builder::new()
+                .tags(tags)
+                .clean(&core_email.get_body().unwrap_or("".to_string()))
+                .to_string();
+            return Ok(a);
+        }
     }
-    Ok(email.get_body().unwrap_or("".to_string()))
+
+    if core_email.ctype.mimetype == "text/plain" {
+        // TODO html escape this.
+        return Ok(core_email.get_body().unwrap_or("".to_string()));
+    }
+    return Ok(String::from("[No valid body found]"));
 }
 
 // pub fn get_attachment(email: &&ParsedMail) -> askama::Result<String> {}
