904dea9b135bf9f60b71d9e842145c73c7d669bc
alex wennerberg <alex@alexwennerberg.com>
2021-07-31 21:11:12 -0700

    Add subpart parsing

--- src/filters.rs
+++ src/filters.rs
@@ -8,5 +8,11 @@ pub fn get_header(email: &&ParsedMail, header: &str) -> askama::Result<String> {
 }
 
 pub fn get_body(email: &&ParsedMail) -> askama::Result<String> {
+    // TODO mimetypes
+    if email.subparts.len() > 0 {
+        return Ok(email.subparts[0].get_body().unwrap_or("".to_string()));
+    }
     Ok(email.get_body().unwrap_or("".to_string()))
 }
+
+// pub fn get_attachment(email: &&ParsedMail) -> askama::Result<String> {}
