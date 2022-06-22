b95e97834ce41643a0d27a459d6914d560dd4db7
Johann150 <johann.galle@protonmail.com>
2022-06-22 15:21:30 +0200

    refactor: remove templating engine

--- src/templates/gmi.rs
+++ src/templates/gmi.rs
@@ -4,22 +4,24 @@ use crate::models::*;
 use crate::templates::PAGE_SIZE;
 use crate::time::Date;
 use crate::util::*;
-use nanotemplate::template;
+use std::fmt::Write;
+
+macro_rules! lines {
+    ($($line:literal),+ $(,)?) => {
+        concat!(
+            $($line, "\r\n"),+
+        )
+    };
+}
 
 impl Lists {
     pub fn to_gmi(&self) -> String {
         let mut lists = String::new();
         for list in &self.lists {
-            lists.push_str(&format!("=> ./{0}/ {0}\n", &h(&list.config.name)));
+            // writing to a string so discarding errors is fine
+            let _ = write!(lists, "=> ./{0}/ {0}\r\n", h(&list.config.name));
         }
-        // this looks stupid ok I know
-        template(
-            r#"# Mail Archives
-{lists}
-         "#,
-            &[("lists", &lists)],
-        )
-        .unwrap()
+        format!("# Mail Archives\r\n{lists}")
     }
 }
 
@@ -32,43 +34,34 @@ impl List {
             .enumerate()
             .map(|(n, thread_topics)| {
                 let mut threads = format!(
-                    "## {0}\n{1}\n=>mailto:{2} {2}\n",
+                    "## {0}\r\n{1}\r\n=>mailto:{2} {2}\r\n",
                     self.config.name, self.config.description, self.config.email
                 );
                 for thread in thread_topics {
-                    threads.push_str(
-                        // TODO reuse with html templates?
-                        &template(
-                            r#"
-=> threads/{path_id}.gmi {subject}
-{preview}
-{from} | {replies} replies | {date}
-"#,
-                            &[
-                                (
-                                    "path_id",
-                                    &h(thread.message.pathescape_msg_id().to_str().unwrap()),
-                                ),
-                                ("subject", &h(&thread.message.subject)),
-                                ("replies", &thread.reply_count.to_string()),
-                                ("preview", &h(&thread.message.preview)),
-                                ("date", &h(&Date::from(thread.last_reply).ymd())),
-                                (
-                                    "from",
-                                    &h(&thread. // awkawrd
-                                message.from
-                                .name
-                                .clone()
-                                .unwrap_or(thread.message.from.address.clone())
-                                .clone()),
-                                ),
-                            ],
-                        )
-                        .unwrap(),
+                    // writing to a string so discarding errors is fine
+                    let _ = write!(
+                        threads,
+                        lines!(
+                            "=>threads/{path_id}.gmi {subject}",
+                            "{preview}",
+                            "{from} | {replies} replies | {date}",
+                        ),
+                        path_id = h(thread.message.pathescape_msg_id().to_str().unwrap()),
+                        subject = h(&thread.message.subject),
+                        replies = thread.reply_count,
+                        preview = h(&thread.message.preview),
+                        date = h(&Date::from(thread.last_reply).ymd()),
+                        from = h(thread
+                            .message
+                            .from
+                            .name
+                            .as_ref()
+                            .unwrap_or(&thread.message.from.address)),
                     );
                 }
-                if n + 1 <= page_count {
-                    threads.push_str(&format!("\n=> index-{}.gmi next", n + 1));
+                if n < page_count {
+                    // writing to a string so discarding errors is fine
+                    let _ = write!(threads, "\r\n=> index-{}.gmi next", n + 1);
                 }
                 threads
             })
@@ -82,58 +75,57 @@ impl Thread {
         for msg in &self.messages {
             let mut optional_headers = String::new();
             if let Some(irt) = &msg.in_reply_to {
-                optional_headers.push_str(&format!("\nIn-Reply-To: {}", &h(&irt)));
+                // writing to a string so discarding errors is fine
+                let _ = write!(optional_headers, "\r\nIn-Reply-To: {}", h(irt));
             }
-            // TODO no copy pasta
-            let cc_string = &h(&msg
-                .cc
-                .iter()
-                .map(|t| t.to_string())
-                .collect::<Vec<String>>()
-                .join(", "));
-            if msg.cc.len() > 0 {
-                optional_headers.push_str(&format!("\nCc: {}", cc_string));
+            if !msg.cc.is_empty() {
+                // writing to a string so discarding errors is fine
+                let _ = write!(
+                    optional_headers,
+                    "\r\nCc: {}",
+                    h(&msg
+                        .cc
+                        .iter()
+                        .map(|t| t.to_string())
+                        .collect::<Vec<String>>()
+                        .join(", "))
+                );
             }
             let body = match msg.flowed {
                 true => unformat_flowed(&msg.body),
                 false => msg.body.clone(),
             };
-            let msg = template(
-                r#"
-## {subject}
-From: {from}
-Date: {date}
-Message-Id: {msg_id}
-To: {to}{optional_headers}
-=> {mailto} Reply
-=> ../messages/{msg_path}.mbox Export
---------------------------------------
-{body}
-"#,
-                &[
-                    ("subject", &h(&msg.subject)),
-                    ("date", &h(&msg.date)),
-                    ("msg_id", &h(&msg.id)),
-                    (
-                        "to",
-                        &h(&msg
-                            .to
-                            .iter()
-                            .map(|t| t.to_string())
-                            .collect::<Vec<String>>()
-                            .join(", ")),
-                    ),
-                    ("optional_headers", &optional_headers),
-                    ("from", &h(&msg.from.address)),
-                    ("mailto", &h(&msg.mailto)),
-                    ("msg_path", &h(msg.pathescape_msg_id().to_str().unwrap())),
-                    // TODO escape # in body?
-                    // TODO only unformat flowed if flowed is true
-                    ("body", &body),
-                ],
-            )
-            .unwrap();
-            out.push_str(&msg);
+            // writing to a string so discarding errors is fine
+            let _ = write!(
+                out,
+                lines!(
+                    "## {subject}",
+                    "From: {from}",
+                    "Date: {date}",
+                    "Message-Id: {msg_id}",
+                    "To: {to}{optional_headers}",
+                    "=> {mailto} Reply",
+                    "=> ../messages/{msg_path}.mbox Export",
+                    "--------------------------------------",
+                    "{body}",
+                ),
+                subject = h(&msg.subject),
+                date = h(&msg.date),
+                msg_id = h(&msg.id),
+                to = h(&msg
+                    .to
+                    .iter()
+                    .map(|t| t.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ")),
+                optional_headers = optional_headers,
+                from = h(&msg.from.address),
+                mailto = h(&msg.mailto),
+                msg_path = h(msg.pathescape_msg_id().to_str().unwrap()),
+                // TODO escape # in body?
+                // TODO only unformat flowed if flowed is true
+                body = body,
+            );
         }
         out
     }
