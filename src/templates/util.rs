ca378cc504e105f262729630029b1efc67f9759e
Johann150 <johann.galle@protonmail.com>
2022-06-22 15:48:05 +0200

    remove unused xml_escape

--- src/templates/util.rs
+++ src/templates/util.rs
@@ -1,22 +1,20 @@
 // less efficient, easier api
 pub fn xml_safe(text: &str) -> String {
     // note we escape more than we need to
-    let mut dest = Vec::new();
-    xml_escape(text, &mut dest);
-    std::str::from_utf8(&dest).unwrap().to_owned()
-}
-
-pub fn xml_escape(text: &str, dest: &mut Vec<u8>) {
-    for c in text.bytes() {
-        match c {
-            b'&' => dest.extend_from_slice(b"&amp;"),
-            b'<' => dest.extend_from_slice(b"&lt;"),
-            b'>' => dest.extend_from_slice(b"&gt;"),
-            b'"' => dest.extend_from_slice(b"&quot;"),
-            b'\'' => dest.extend_from_slice(b"&#39;"),
-            // Quick and dirty email obfuscation
-            b'@' => dest.extend_from_slice(b"&#x40;"),
-            _ => dest.push(c),
-        }
-    }
+    text.chars()
+        .map(|c| {
+            match c {
+                '&' => "&amp;",
+                '<' => "&lt;",
+                '>' => "&gt;",
+                '"' => "&quot;",
+                '\'' => "&#39;",
+                // Quick and dirty email obfuscation
+                '@' => "&#x40;",
+                // need an explicit return here because c is not a &str
+                _ => return c.to_string(),
+            }
+            .to_string()
+        })
+        .collect::<String>()
 }
